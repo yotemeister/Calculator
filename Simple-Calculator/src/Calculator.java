cd16ab83d1404ab3327391159089dd86d414b16b
yotemeister <46034253+yotemeister@users.noreply.github.com>
2018-12-21 08:03:58 -0500

    Update Calculator.java

--- Simple-Calculator/src/Calculator.java
+++ Simple-Calculator/src/Calculator.java
@@ -1,4 +1,5 @@
 
+package Yeet;
 
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -10,7 +11,7 @@ import javax.swing.JTextField;
 
 public class Calculator {
 
-        JButton add = new JButton("Add");
+        JButton add = new JButton("Addition");
         JButton mult = new JButton("Multiply");
         JButton div = new JButton("Division");
         JButton sub = new JButton("Subtraction");
@@ -18,20 +19,21 @@ public class Calculator {
         JTextField input1 = new JTextField(20);
         JTextField input2 = new JTextField(20);
         JTextField input3 = new JTextField(20);
+        JFrame calc = new JFrame();
 
     
     Calculator(){
-        JFrame calc = new JFrame();
         
         Listener listener = new Listener();
         
         FlowLayout layout = new FlowLayout();
         calc.setLayout(layout);
         
-        mult.addActionListener(listener);
+        
         add.addActionListener(listener);
-        div.addActionListener(listener);
+        mult.addActionListener(listener);
         sub.addActionListener(listener);
+        div.addActionListener(listener);
         
         calc.add(input1);
         calc.add(input2);
@@ -41,7 +43,8 @@ public class Calculator {
         calc.add(div);
         calc.add(sub);
         
-        calc.setSize(500, 600);
+        
+        calc.setSize(500, 800);
         calc.setVisible(true);
         calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calc.setTitle("Calculator");
@@ -56,71 +59,74 @@ public class Calculator {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-        	
-        	JButton button = (JButton) e.getSource();
-        	
-        	if (button.equals(add)) {
-        		//System.out.println("you pressed a button!");       
-                
-                        String text1 = input1.getText();
-                        String text2 = input2.getText();
-                        float num1 = Float.parseFloat(text1);
-                        float num2 = Float.parseFloat(text2);
-
-                        float result = num1 + num2;
-
-                        input3.setText(result+"");
-                	
-        	}
-        	
-        	if (button.equals(mult)) {
-        		
-        		//System.out.println("you pressed the multiplication button");
-        		
-        		String text1 = input1.getText();
-        		String text2 = input2.getText();
-                        float num1 = Float.parseFloat(text1);
-                        float num2 = Float.parseFloat(text2);
-
-                        float result = num1 * num2;
-
-                        input3.setText(result + "");
-        		
-        	}
-        	if (button.equals(div)) {
-        		
-        		//System.out.println("you pressed the multiplication button");
-        		
-        		String text1 = input1.getText();
-        		String text2 = input2.getText();
-                        float num1 = Float.parseFloat(text1);
-                        float num2 = Float.parseFloat(text2);
-
-                        float result = num1 / num2;
-
-                        input3.setText(result + "");
-        		
-        	}
-        	if (button.equals(sub)) {
-        		
-        		//System.out.println("you pressed the multiplication button");
-        		
-        		String text1 = input1.getText();
-        		String text2 = input2.getText();
-                        float num1 = Float.parseFloat(text1);
-                        float num2 = Float.parseFloat(text2);
-
-                        float result = num1 - num2;
-
-                        input3.setText(result + "");
-        		
-        	}
         
+            JButton button = (JButton) e.getSource();
+            
+            if (input1.getText().equals("")){
+                input1.setText("0");
+                input2.setText("0");
+            }
+
+            
+            if (button.equals(add)){
+                String text1 = input1.getText();
+                String text2 = input2.getText();
+                float num1 = Float.parseFloat(text1);
+                float num2 = Float.parseFloat(text2);
+
+                float result = num1 + num2;
+
+                input3.setText(result+"");
+                
+            }
+            
+            if (button.equals(mult)){
+                String text1 = input1.getText();
+                String text2 = input2.getText();
+                float num1 = Float.parseFloat(text1);
+                float num2 = Float.parseFloat(text2);
+
+                float result = num1 * num2;
+
+                input3.setText(result+"");
+
+            }
+            
+            if (button.equals(sub)){
+                String text1 = input1.getText();
+                String text2 = input2.getText();
+                float num1 = Float.parseFloat(text1);
+                float num2 = Float.parseFloat(text2);
+
+                float result = num1 - num2;
+
+                input3.setText(result+"");
+
+            }
+            if (button.equals(div)){
+                
+                if (input2.getText().equals("0")){
+                    input3.setText("Cannot Divide by Zero");
+                    return;
+                }
+                
+                String text1 = input1.getText();
+                String text2 = input2.getText();
+                float num1 = Float.parseFloat(text1);
+                float num2 = Float.parseFloat(text2);
+
+                float result = num1 / num2;
+
+                input3.setText(result+"");
+
+            }
+            //System.out.println("you pressed a button!");       
+            
             
         }
         
+        
     }
-    
     
     public static void main(String[] args){
     
